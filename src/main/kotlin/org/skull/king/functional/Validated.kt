6a01fece3a4813c3ede5594bb1ffa6c8e28ebee3
Alexandre Bignalet <a.bignalet@betclicgroup.com>
2020-11-16 19:51:13 +0100

    do not run mock firebase during stage

--- src/main/kotlin/org/skull/king/functional/Validated.kt
+++
@@ -1,8 +0,0 @@
-package org.skull.king.functional
-
-sealed class Validated<out A, out B>()
-
-data class Invalid<A>(val err: A) : Validated<A, Nothing>()
-data class Valid<B>(val value: B) : Validated<Nothing, B>()
-
-
